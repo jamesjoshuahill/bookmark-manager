2069515bcd8690b626fd7dc5956becc1c4724f8e
Josh Hill <jamesjoshuahill@gmail.com>
2013-09-25 16:50:31 +0100

    Fixes to flash for errors in user sign up

--- lib/user.rb
+++ lib/user.rb
@@ -7,7 +7,6 @@ class User
   attr_accessor :password_confirmation
 
   validates_confirmation_of :password, :message => "Sorry, your passwords didn't match"
-  validates_uniqueness_of :email
 
   property :id, Serial
   property :email, String, :unique => true, :message => "This email is already taken"
