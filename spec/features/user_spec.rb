fcc2ca3e279d80d5c72e4934203a0828683f50af
Josh Hill <jamesjoshuahill@gmail.com>
2013-09-25 15:15:59 +0100

    Added password confirmation to user sign up

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -3,9 +3,15 @@ require 'spec_helper'
 feature 'User signs up' do
 
   scenario 'when being logged out' do
-    expect{ sign_up }.to change(User, :count).by 1
+    expect { sign_up }.to change(User, :count).by 1
     expect(page).to have_content("Welcome, jess@puppy.com")
     expect(User.first.email).to eq("jess@puppy.com")
+  end
+
+  scenario "with a password that doesn't match" do
+    expect {
+      sign_up('a@a.com', 'pass', 'wrong')
+    }.to change(User, :count).by 0
   end
 
 end
--- spec/support/features/session_helpers.rb
+++ spec/support/features/session_helpers.rb
@@ -19,10 +19,11 @@ module Features
       end      
     end
 
-    def sign_up(email="jess@puppy.com", password="cheese!")
+    def sign_up(email="jess@puppy.com", password="cheese!", password_confirmation="cheese!")
       visit '/users/new'
       fill_in 'email', :with => email
       fill_in 'password', :with => password
+      fill_in 'password_confirmation', :with => password_confirmation
       click_button 'Sign up'
     end
 
